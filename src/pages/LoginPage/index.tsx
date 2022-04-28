a918cf5126f6b46c8d9955d9c9beeaeb5283fdda
LionyxML <rahul.juliato@gmail.com>
2022-04-27 23:41:15 -0300

    feat: login page

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,23 +1,134 @@
 import React from "react";
-import { Paper } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  Link,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import { SContainer } from "./styles";
 
 export const LoginPage: React.FC = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
   return (
     <SContainer>
-      CC site comming soon...
-      <Paper
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: 300,
-          width: 300,
-        }}
-      >
-        {" "}
-        ;)
-      </Paper>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage:
+              "url(https://images.pexels.com/photos/3183183/pexels-photo-3183183.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h2" sx={{ m: 4 }}>
+              CC System
+            </Typography>
+            <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Log in
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Endereço de e-mail"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Senha"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Lembrar meus dados"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Entrar
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="https://google.com" variant="body2">
+                    Esqueceu sua senha?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="https://google.com" variant="body2">
+                    Ainda não tem sua conta? Registre-se.
+                  </Link>
+                </Grid>
+              </Grid>
+              <Typography
+                sx={{ mt: 5 }}
+                variant="body2"
+                color="text.secondary"
+                align="center"
+              >
+                {"Copyright © "}
+                <Link color="inherit" href="https://rmjsoftware.com/">
+                  RMJ Software
+                </Link>{" "}
+                {new Date().getFullYear()}
+              </Typography>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
     </SContainer>
   );
 };
