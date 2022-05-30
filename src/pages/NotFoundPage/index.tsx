20b71f99096ed6e7f7c1bf00b05ec3476969874a
LionyxML <rahul.juliato@gmail.com>
2022-05-30 19:12:43 -0300

    feat: adds custom 404 page

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,5 +1,35 @@
+import { Grid, Typography } from "@mui/material";
 import React from "react";
+import { CopyrightLine } from "../../components";
 
 export const NotFoundPage: React.FC = () => {
-  return <p>Not found page!</p>;
+  return (
+    <Grid sx={{ mt: 5 }}>
+      <Typography
+        sx={{ mt: 10 }}
+        variant="h2"
+        color="text.secondary"
+        align="center"
+      >
+        Ooops!
+      </Typography>
+      <Typography
+        sx={{ mt: 5 }}
+        variant="h4"
+        color="text.secondary"
+        align="center"
+      >
+        Página não encontrada.
+      </Typography>
+      <Typography
+        sx={{ mt: 10 }}
+        variant="h5"
+        color="text.secondary"
+        align="center"
+      >
+        Desculpe-nos pelo transtorno :(
+      </Typography>
+      <CopyrightLine />
+    </Grid>
+  );
 };
